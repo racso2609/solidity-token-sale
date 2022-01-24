be28bb8a186139f88141470648bc9e934360b776
Oscar <oscaremiliolugo@gmail.com>
2022-01-23 19:10:29 -0500

    finish end sale implementation test

--- test/TokenSale.js
+++ test/TokenSale.js
@@ -2,11 +2,11 @@ const chai = require("chai");
 const { ethers } = require("hardhat");
 const { expect } = chai;
 const { solidity } = require("ethereum-waffle");
-const { BigNumber, utils } = require("ethers");
+const { utils } = require("ethers");
 const { parseEther } = utils;
 
 const TOTAL_SUPPLY = 1_000_000;
-const TOKEN_PRICE = "0.001"; // 0,001 in wei
+const TOKEN_PRICE = "0.001";
 const TOKENS_AVALIABLE = 750_000;
 
 chai.use(solidity);
@@ -45,13 +45,35 @@ describe("Token Sale", () => {
         })
       ).to.be.revertedWith("wrong eth amount");
     });
+    // ------------------ FALLOOOOO ---------------
     it("fail purchase more tokens than avaliable", async () => {
       const tokenAmount = TOKENS_AVALIABLE + 1;
       await expect(
         tokenSaleInstance.connect(buyer).buyTokens(tokenAmount, {
-          value: tokenAmount * parseEther(TOKEN_PRICE),
+          value: tokenAmount * parseEther(TOKEN_PRICE),  // the problem is the overflow of this operation if you decreae the quantity of token avaliable to 7 it pass normally 
         })
       ).to.be.revertedWith("cannot purchase more tokens than avaliable");
+    });
+    // ------- :( ------------------------
+    it("purchase tokens", async () => {
+      const tokenAmount = 1;
+      await tokenSaleInstance.connect(buyer).buyTokens(tokenAmount, {
+        value: tokenAmount * parseEther(TOKEN_PRICE),
+      });
+      expect(await tokenInstance.balanceOf(buyer.address)).to.equal(
+        tokenAmount
+      );
+    });
+  });
+  describe("End sale", () => {
+    it("end sale fail you are not a admin", async () => {
+      await expect(tokenSaleInstance.connect(buyer).endSale()).to.be.reverted;
+    });
+    it("end sale fail you are not a admin", async () => {
+      await tokenSaleInstance.connect(admin).endSale();
+      expect(await tokenInstance.balanceOf(admin.address)).to.equal(
+        TOTAL_SUPPLY
+      );
     });
   });
 });
